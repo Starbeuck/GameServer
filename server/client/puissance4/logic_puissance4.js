fc5cac9508068532ec0727370434749523c16d26
Solenn KEROULLAS <keroullasolenn@gmail.com>
2018-05-23 15:31:09 +0200

    Join Two by Game

--- server/client/puissance4/logic_puissance4.js
+++ server/client/puissance4/logic_puissance4.js
@@ -1,8 +1,9 @@
-
 var randomstring = require('randomstring');
 
 var rooms = [];
 var newroom;
+var newroomTwocount = 0;
+var alotOfRoomOfTwo = [];
 
 module.exports = {
 
@@ -11,5 +12,22 @@ module.exports = {
     rooms.push(newroom);
     socket.join(newroom);
     console.log('Le client qui joue au puissance 4 | id : ' + socket.id + ' a rejoint la room : ' + newroom);
+  },
+
+  joinTwo: function (socket) {
+    newroomTwocount = newroomTwocount + 1;
+    console.log(newroomTwocount);
+    if (newroomTwocount % 2 == 1) {
+      newroom = randomstring.generate(5);
+      alotOfRoomOfTwo.push(newroom);
+      rooms.push(newroom);
+      socket.join(newroom);
+      console.log('Le client qui joue au P4 ' + socket.id + ' a rejoint la room ' + newroom);
+    } else {
+      newroom = alotOfRoomOfTwo[alotOfRoomOfTwo.length - 1];
+      socket.join(newroom);
+      console.log('Le client qui joue au P4 ' + socket.id + ' a rejoint la room ' + newroom + 'du jeu ' + name);
+    }
   }
+
 };
