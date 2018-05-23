96949cb22ecd5e6114b908d6d7a28b778699a88e
elChipardo <hugues.connan@gmail.com>
2018-05-23 14:02:29 +0200

    grille morpion ok + mettre croix/rond les uns après les autres ok

--- client/morpion/main.js
+++ client/morpion/main.js
@@ -6,10 +6,15 @@ var height = 600;
 canvas.width=width;
 canvas.height=height;
 
-// Code temporaire pour tester le DnD
-//new DnD(canvas);
+var joueurCour = 1;
+
+
+var buttonCirc = document.getElementById('butCirc');
+var buttonCross = document.getElementById('butCrss');
+
 ctx.fillStyle = '#F0F0F0'; // set canvas' background color
 ctx.fillRect(0, 0, canvas.width, canvas.height);  // now fill the canvas
+
 
 var x, y;
 for (x=0;x<=width;x+=width/3) {
@@ -20,20 +25,141 @@ for (x=0;x<=width;x+=width/3) {
         ctx.moveTo(0, y);
         ctx.lineTo(width, y);
         ctx.stroke();
+
     }
 }
+function getMousePos(event) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: event.clientX - rect.left,
+    y: event.clientY - rect.top
+  };
+}
+
+function round(event){
+
+    var mousePos = getMousePos(event);
+    ctx.lineWidth = "5";
+    var xMouse = mousePos.x;
+    var yMouse = mousePos.y;
+    console.log(xMouse);
+    console.log(yMouse);
+    if(xMouse>=0 && xMouse<(width/3)){
+        if(yMouse>=0 && yMouse<(width/3)){
+            ctx.beginPath();
+            if(joueurCour == 1) {
+                ctx.arc(100, 100, 70, 0, 2 * Math.PI);
+            }
+            else{
+                crossed(30, 30, 170, 170);
+            }
+            ctx.stroke();
+        }
+        else if(yMouse>=(width/3) && yMouse<(2*(width/3))){
+            ctx.beginPath();
+            if(joueurCour == 1) {
+                ctx.arc(100, 300, 70, 0, 2 * Math.PI);
+            }
+            else{
+                crossed(30, 230, 170, 370);
+            }
+            ctx.stroke();
+        }
+        else{
+            ctx.beginPath();
+            if(joueurCour == 1) {
+                ctx.arc(100, 500, 70, 0, 2 * Math.PI);
+            }
+            else{
+                crossed(30,430, 170, 570);
+            }
+            ctx.stroke();
+        }
+    }
+    else if(xMouse>=(width/3) && xMouse<(2*(width/3))){
+        if(yMouse>=0 && yMouse<(width/3)) {
+            ctx.beginPath();
+            if (joueurCour == 1) {
+                ctx.arc(300, 100, 70, 0, 2 * Math.PI);
+            }
+            else{
+                crossed(230, 30, 370, 170);
+            }
+            ctx.stroke();
+        }
+        else if(yMouse>=(width/3) && yMouse<(2*(width/3))){
+            ctx.beginPath();
+            if(joueurCour == 1) {
+                ctx.arc(300, 300, 70, 0, 2 * Math.PI);
+            }
+            else{
+                crossed(230, 230, 370, 370);
+            }
+            ctx.stroke();
+        }
+        else{
+            ctx.beginPath();
+            if(joueurCour == 1) {
+                ctx.arc(300, 500, 70, 0, 2 * Math.PI);
+            }
+            else{
+                crossed(230, 430, 370, 570);
+            }
+            ctx.stroke();
+        }
+    }
+    else{
+        if(yMouse>=0 && yMouse<(width/3)){
+            ctx.beginPath();
+            if(joueurCour == 1) {
+                ctx.arc(500, 100, 70, 0, 2 * Math.PI);
+            }
+            else{
+                crossed(430, 30, 570, 170);
+            }
+            ctx.stroke();
+        }
+        else if(yMouse>=(width/3) && yMouse<(2*(width/3))){
+            ctx.beginPath();
+            if(joueurCour == 1) {
+                ctx.arc(500, 300, 70, 0, 2 * Math.PI);
+            }
+            else{
+                crossed(430, 230, 570, 370)
+            }
+            ctx.stroke();
+        }
+        else{
+            ctx.beginPath();
+            if(joueurCour == 1) {
+                ctx.arc(500, 500, 70, 0, 2 * Math.PI);
+            }
+            else{
+                crossed(430, 430, 570, 570);
+            }
+            ctx.stroke();
+        }
+    }
+    joueurCour = 3 - joueurCour;
+}
+
+function crossed(xStart, yStart, xEnd, yEnd){
+  ctx.beginPath();
+  ctx.moveTo(xStart,yStart);
+  ctx.lineTo(xEnd, yEnd);
+  ctx.stroke();
+  ctx.moveTo(xStart, yEnd);
+  ctx.lineTo(xEnd, yStart);
+  ctx.stroke();
+}
+
+canvas.addEventListener("click", round);
+// Code temporaire pour tester le DnD
+//new DnD(canvas);
+
 /////
 
 // Code temporaire pour tester l'affiche de la vue
 //var rec = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
 //rec.paint(ctx);
-//var line = new Line(10, 20, 50, 100, 5, '#00CCC0');
-//line.paint(ctx);
-// tester également Dessin.
-////
-
-// Code final à utiliser pour manipuler Pencil.
-//var drawing = new Drawing();
-//var pencil = new Pencil(ctx, drawing, canvas);
-//drawing.paint(ctx, canvas);
-
+//var line = new Line(10, 20, 50, 100, 5, '#00CCC0')
