8bbc8d89e691b693dc4f66018b1912c23e0f6ec0
Solenn KEROULLAS <keroullasolenn@gmail.com>
2018-05-23 15:11:54 +0200

    Join Alone by game

---
+++ server/client/Demineur/logic_demineur.js
@@ -0,0 +1,12 @@
+var rooms = [];
+var newroom;
+
+module.exports = {
+
+  joinAlone: function (socket) {
+    newroom = randomstring.generate(5);
+    rooms.push(newroom);
+    clientSocket.join(newroom);
+    console.log('Le client qui joue au démineur' + clientSocket.id + ' a rejoint la room ' + newroom + 'du jeu ' + name);
+  }
+};
