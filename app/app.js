21e9c7bc9b4f41dbd64a1b6b98d526bb36cb00dd
Solenn KEROULLAS <keroullasolenn@gmail.com>
2018-05-24 15:50:53 +0200

    init file test

--- app/app.js
+++ app/app.js
@@ -3,15 +3,16 @@ var express = require('express');
 var http = require('http').Server(app);
 var path = require('path');
 
+// enable use file client
 app.use(express.static('public'));
 
-
+// init root path
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/public', 'accueil.html'));
- // res.sendFile(path.join(__dirname, 'index.html'));
+ // res.sendFile(path.join(__dirname, '/public', 'accueil.html'));
+  res.sendFile(path.join(__dirname, 'test.html'));
 });
 
-
+// display on port 1234
 http.listen(1234, function(){
   console.log('listening on 1234');
 });
