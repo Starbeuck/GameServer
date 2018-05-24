eaa0ac226798026937d9c82c7ab9eac373a0e53c
Solenn KEROULLAS <keroullasolenn@gmail.com>
2018-05-24 15:49:03 +0200

    init server + add img

--- app/app.js
+++ app/app.js
@@ -1,9 +1,16 @@
 var app = require('express')();
+var express = require('express');
 var http = require('http').Server(app);
+var path = require('path');
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public', 'accueil.html'));
+ // res.sendFile(path.join(__dirname, 'index.html'));
 });
+
 
 http.listen(1234, function(){
   console.log('listening on 1234');
