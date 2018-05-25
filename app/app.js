38b5b9228052da710d19c47a4b677735801096a9
FofoDev <faustiine35@gmail.com>
2018-05-25 15:06:56 +0200

    ajout commentaires

--- app/app.js
+++ app/app.js
@@ -5,9 +5,18 @@ var app = express();
 
 const Game = require('./Game.js');
 const Action = require('./Action.js');
+const morpion_play = require('./public/morpion/main.js');
+
 var allGames = [];
 
-// TEST CLASSES
+// get response from client
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// enable use file client
+app.use(express.static('public'));
+
+// -------------------------------------------- TEST CLASSES ---------------------------------------------
 let newAction = new Action('{"x":0, "y":10}');
 
 console.log(newAction.toJson());
@@ -16,34 +25,27 @@ console.log(newAction.toString());
 let newGame = new Game('morpion');
 
 console.log(newGame.toJson());
-// FIN TEST CLASSES
+// -------------------------------------------- FIN TEST CLASSES ------------------------------------------
 
-// get response from client
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(bodyParser.json());
-
-// enable use file client
-app.use(express.static('public'));
 
 // init root path
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   // res.sendFile(path.join(__dirname, '/public', 'accueil.html'));
   res.sendFile(path.join(__dirname, '/public', 'test.html'));
 });
 
-// post request
-app.post('/game', function (req, res) {
+// ----------------------------------- ROUTE GAME -----------------------------------
+
+app.post('/game', function(req, res) {
   // Possibilité 1 : création partie
-  if (req.body.typeGame != undefined){
-    var typeGame=req.body.typeGame;
-    console.log("TypeGame = "+typeGame);
+  if (req.body.typeGame != undefined) {
+    var typeGame = req.body.typeGame;
+    console.log("TypeGame = " + typeGame);
     var newGame = new Game(typeGame);
     allGames.push(newGame);
     res.send(newGame.toString());
-  // Possibilité 2 : jouer dans une partie
-}else if((req.body.game !=  undefined) && (req.body.action != undefined)){
+    // Possibilité 2 : jouer dans une partie
+  } else if ((req.body.game != undefined) && (req.body.action != undefined)) {
     var game = new Game('');
     game.fromJson(req.body.game);
     var action = new Action(req.body.action);
@@ -54,18 +56,22 @@ app.post('/game', function (req, res) {
   }
 });
 
-app.post('/idGame', function (req, res) {
-  var idGame = req.body.idGame;
-  var thisGame;
-  for (let i = 0; i < allGames.length; i++) {
-    if (allGames[i].id == idGame) {
-      thisGame = allGames[i];
+// ----------------------------------- ROUTE IDGAME -----------------------------------
+
+app.post('/idGame', function(req, res) {
+  if (req.body.idGame != undefined) {
+    var idGame = req.body.idGame;
+    var thisGame;
+    for (let i = 0; i < allGames.length; i++) {
+      if (allGames[i].id == idGame) {
+        thisGame = allGames[i];
+      };
     };
-  };
-  res.send(thisGame);
+    res.send(thisGame);
+  }
 });
 
 //open port
-app.listen(1234, function () {
+app.listen(1234, function() {
   console.log("Started on PORT 1234");
 })
