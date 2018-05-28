c1e25bd1ddd4220cd482e7d4f4d865599a1597e5
FofoDev <faustiine35@gmail.com>
2018-05-28 15:58:14 +0200

    ??

--- app/public/morpion/morpion_IA.js
+++ app/public/morpion/morpion_IA.js
@@ -1,263 +1,126 @@
 const Game = require('../Game.js');
 const Action = require('../Action.js');
 
-var nextAction =function(game){
-    //var action = new Action('{"x":0, "y":0}');
-    var action = playIA(game, 0);
+var huPlayer = 1,
+    aiPlayer = 2,
+    nbCol = 3,
+    nbLi = 3;
+
+function convertBoard(grid) {
+    var board = [];
+
+    for(var v=0 ; v<nbCol ; v++) {
+        for(var w=0 ; w<nbLi ; w++) {
+            board[w*nbCol+v] = grid[v][w];
+        }
+    }
+    return board;
+}
+
+var nextAction = function(game){
+
+    var origBoard = convertBoard(game.grid);
+    var action = minimax(origBoard, 2);
+
     return action;
 }
 
-function playIA(game, depth) {
-    var max = -10000,
-        tmp,
-        maxI,
-        maxJ,
-        i,
-        j;
-
-    for(i=0 ; i<3 ; i++) {
-        for(j=0 ; j<3 ; j++) {
-            if(game.grid[i][j] === 0) {
-                game.grid[i][j] = 1;
-                tmp = getMin(game, depth-1);
-
-                if(tmp > max) {
-                    max = tmp;
-                    maxI = i;
-                    maxJ = j;
-                }
-
-                game.grid[i][j] = 0;
-            }
-        }
-    }
-
-    // Ici on ne veut pas jouer mais retourner l'action associée
-    //game.grid[i][j] = 1;
-    return new Action('{"x":'+i+', "y":'+j+', "currentPlayer":2}');
+function emptyIndexies(board){
+    return  board.filter(s => s != 1 && s != 2);
 }
 
-function getMax(game, depth) {
-    if(depth === 0 || getWinner(game) !== 0) {
-        return computePossibilities(game);
-    }
-
-    var max = -10000,
-        i,
-        j,
-        tmp;
-
-    for(i=0 ; i<3 ; i++) {
-        for(j=0 ; j<3 ; j++) {
-            if(game.grid[i][j] === 0)
-            {
-                game.grid[i][j] = 2;
-                tmp = getMin(game, depth-1);
-
-                if(tmp > max)
-                {
-                    max = tmp;
-                }
-                game.grid[i][j] = 0;
-            }
-        }
-    }
-
-    return max;
-}
-
-function getMin(game, depth) {
-    if(depth === 0 || getWinner(game) !== 0) {
-        return computePossibilities(game);
-    }
-
-    var min = 10000,
-        i,
-        j,
-        tmp;
-
-    for(i=0 ; i<3 ; i++) {
-        for(j=0 ; j<3 ; j++) {
-            if(game.grid[i][j] === 0)
-            {
-                game.grid[i][j] = 1;
-                tmp = getMax(game, depth-1);
-
-                if(tmp < min)
-                {
-                    min = tmp;
-                }
-                game.grid[i][j] = 0;
-            }
-        }
-    }
-
-    return min;
-}
-
-//Compte le nombre de séries de n pions alignés de chacun des joueurs
-function nb_series(game, series_j1, series_j2, n) {
-    var count1,
-        count2,
-        i,
-        j;
-
-    series_j1 = 0;
-    series_j2 = 0;
-
-    count1 = 0;
-    count2 = 0;
-
-    //Diagonale descendante
-    for(i=0;i<3;i++) {
-        if(game.grid[i][i] === 1) {
-            count1++;
-            count2 = 0;
-
-            if(count1 === n) {
-                series_j1++;
-            }
-        } else if(game.grid[i][i] === 2) {
-            count2++;
-            count1 = 0;
-
-            if(count2 === n)
-            {
-                series_j2++;
-            }
-        }
-    }
-
-    count1 = 0;
-    count2 = 0;
-
-    //Diagonale montante
-    for(i=0;i<3;i++){
-        if(game.grid[i][2-i] === 1){
-            count1++;
-            count2 = 0;
-
-            if(count1 === n){
-                series_j1++;
-            }
-        } else if(game.grid[i][2-i] === 2) {
-            count2++;
-            count1 = 0;
-
-            if(count2 === n) {
-                series_j2++;
-            }
-        }
-    }
-
-    //En ligne
-    for(i=0;i<3;i++) {
-        count1 = 0;
-        count2 = 0;
-
-        //Horizontalement
-        for(j=0;j<3;j++) {
-            if(game.grid[i][j] === 1) {
-                count1++;
-                count2 = 0;
-
-                if(count1 === n) {
-                    series_j1++;
-                }
-            }
-            else if(game.grid[i][j] === 2) {
-                count2++;
-                count1 = 0;
-
-                if(count2 === n) {
-                    series_j2++;
-                }
-            }
-        }
-
-        count1 = 0;
-        count2 = 0;
-
-        //Verticalement
-        for(j=0;j<3;j++) {
-            if(game.grid[j][i] === 1) {
-                count1++;
-                count2 = 0;
-
-                if(count1 === n) {
-                    series_j1++;
-                }
-            } else if(game.grid[j][i] === 2) {
-                count2++;
-                count1 = 0;
-
-                if(count2 === n) {
-                    series_j2++;
-                }
-            }
-        }
-    }
-}
-
-function computePossibilities(game) {
-    var winner,
-        nbPawn = 0,
-        i,
-        j;
-
-    //On compte le nombre de pions présents sur le plateau
-    for(i=0 ; i<3 ; i++) {
-        for(j=0 ; j<3 ; j++) {
-            if(game.grid[i][j] !== 0) {
-                nbPawn++;
-            }
-        }
-    }
-
-    if( (winner = getWinner(game)) !== 0) {
-        if(winner === 1) {
-            return 1000 - nbPawn;
-        } else if(winner === 2) {
-            return -1000 + nbPawn;
-        } else {
-            return 0;
-        }
-    }
-
-    //On compte le nombre de séries de 2 pions alignés de chacun des joueurs
-    var series_j1 = 0,
-        series_j2 = 0;
-
-    nb_series(game, series_j1, series_j2, 2);
-
-    return (series_j1 - series_j2);
-}
-
-function getWinner(game) {
-    var i,
-        j,
-        j1,
-        j2;
-
-    nb_series(game, j1, j2, 3);
-
-    if(j1) {
-        return 1;
-    } else if(j2) {
-        return 2;
+function winning(board, player){
+    if (
+        (board[0] == player && board[1] == player && board[2] == player) ||
+        (board[3] == player && board[4] == player && board[5] == player) ||
+        (board[6] == player && board[7] == player && board[8] == player) ||
+        (board[0] == player && board[3] == player && board[6] == player) ||
+        (board[1] == player && board[4] == player && board[7] == player) ||
+        (board[2] == player && board[5] == player && board[8] == player) ||
+        (board[0] == player && board[4] == player && board[8] == player) ||
+        (board[2] == player && board[4] == player && board[6] == player)
+    ) {
+        return true;
     } else {
-        //Si le jeu n'est pas fini et que personne n'a gagné, on renvoie 0
-        for(i=0;i<3;i++) {
-            for(j=0;j<3;j++) {
-                if(game.grid[i][j] === 0) {
-                    return 0;
-                }
+        return false;
+    }
+}
+
+// the main minimax function
+function minimax(newBoard, player){
+
+    //available spots
+    var availSpots = emptyIndexies(newBoard);
+
+    // checks for the terminal states such as win, lose, and tie
+    //and returning a value accordingly
+    if (winning(newBoard, huPlayer)){
+        return {score:-10};
+    }
+    else if (winning(newBoard, aiPlayer)){
+        return {score:10};
+    }
+    else if (availSpots.length === 0){
+        return {score:0};
+    }
+
+    // an array to collect all the objects
+    var moves = [];
+
+    // loop through available spots
+    for (var i = 0; i < availSpots.length; i++){
+        //create an object for each and store the index of that spot
+        var move = {};
+        move.index = newBoard[availSpots[i]];
+
+        // set the empty spot to the current player
+        newBoard[availSpots[i]] = player;
+
+        /*collect the score resulted from calling minimax
+          on the opponent of the current player*/
+        if (player == aiPlayer){
+            var result = minimax(newBoard, huPlayer);
+            move.score = result.score;
+        }
+        else{
+            var result = minimax(newBoard, aiPlayer);
+            move.score = result.score;
+        }
+
+        // reset the spot to empty
+        newBoard[availSpots[i]] = move.index;
+
+        // push the object to the array
+        moves.push(move);
+    }
+
+    // if it is the computer's turn loop over the moves and choose the move with the highest score
+    var bestMove;
+    if(player === aiPlayer){
+        var bestScore = -10000;
+        for(var i = 0; i < moves.length; i++){
+            if(moves[i].score > bestScore){
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }else{
+
+        // else loop over the moves and choose the move with the lowest score
+        var bestScore = 10000;
+        for(var i = 0; i < moves.length; i++){
+            if(moves[i].score < bestScore){
+                bestScore = moves[i].score;
+                bestMove = i;
             }
         }
     }
 
-    //Si le jeu est fini et que personne n'a gagné, on renvoie 3
-    return 3;
-}
+    // return the chosen move (object) from the moves array
+    //return moves[bestMove];
 
-module.exports = nextAction;
+    var x = (moves[bestMove].index%nbCol),
+        y = parseInt(moves[bestMove].index/nbCol);
+
+    return new Action('{"x":'+x+', "y":'+y+', "currentPlayer":2}');
+}
---
+++ app/public/morpion/morpion_IA_old.js
@@ -0,0 +1,263 @@
+const Game = require('../Game.js');
+const Action = require('../Action.js');
+
+var nextAction =function(game){
+    //var action = new Action('{"x":0, "y":0}');
+    var action = playIA(game, 0);
+    return action;
+}
+
+function playIA(game, depth) {
+    var max = -10000,
+        tmp,
+        maxI,
+        maxJ,
+        i,
+        j;
+
+    for(i=0 ; i<3 ; i++) {
+        for(j=0 ; j<3 ; j++) {
+            if(game.grid[i][j] === 0) {
+                game.grid[i][j] = 1;
+                tmp = getMin(game, depth-1);
+
+                if(tmp > max) {
+                    max = tmp;
+                    maxI = i;
+                    maxJ = j;
+                }
+
+                game.grid[i][j] = 0;
+            }
+        }
+    }
+
+    // Ici on ne veut pas jouer mais retourner l'action associée
+    //game.grid[i][j] = 1;
+    return new Action('{"x":'+i+', "y":'+j+', "currentPlayer":2}');
+}
+
+function getMax(game, depth) {
+    if(depth === 0 || getWinner(game) !== 0) {
+        return computePossibilities(game);
+    }
+
+    var max = -10000,
+        i,
+        j,
+        tmp;
+
+    for(i=0 ; i<3 ; i++) {
+        for(j=0 ; j<3 ; j++) {
+            if(game.grid[i][j] === 0)
+            {
+                game.grid[i][j] = 2;
+                tmp = getMin(game, depth-1);
+
+                if(tmp > max)
+                {
+                    max = tmp;
+                }
+                game.grid[i][j] = 0;
+            }
+        }
+    }
+
+    return max;
+}
+
+function getMin(game, depth) {
+    if(depth === 0 || getWinner(game) !== 0) {
+        return computePossibilities(game);
+    }
+
+    var min = 10000,
+        i,
+        j,
+        tmp;
+
+    for(i=0 ; i<3 ; i++) {
+        for(j=0 ; j<3 ; j++) {
+            if(game.grid[i][j] === 0)
+            {
+                game.grid[i][j] = 1;
+                tmp = getMax(game, depth-1);
+
+                if(tmp < min)
+                {
+                    min = tmp;
+                }
+                game.grid[i][j] = 0;
+            }
+        }
+    }
+
+    return min;
+}
+
+//Compte le nombre de séries de n pions alignés de chacun des joueurs
+function nb_series(game, series_j1, series_j2, n) {
+    var count1,
+        count2,
+        i,
+        j;
+
+    series_j1 = 0;
+    series_j2 = 0;
+
+    count1 = 0;
+    count2 = 0;
+
+    //Diagonale descendante
+    for(i=0;i<3;i++) {
+        if(game.grid[i][i] === 1) {
+            count1++;
+            count2 = 0;
+
+            if(count1 === n) {
+                series_j1++;
+            }
+        } else if(game.grid[i][i] === 2) {
+            count2++;
+            count1 = 0;
+
+            if(count2 === n)
+            {
+                series_j2++;
+            }
+        }
+    }
+
+    count1 = 0;
+    count2 = 0;
+
+    //Diagonale montante
+    for(i=0;i<3;i++){
+        if(game.grid[i][2-i] === 1){
+            count1++;
+            count2 = 0;
+
+            if(count1 === n){
+                series_j1++;
+            }
+        } else if(game.grid[i][2-i] === 2) {
+            count2++;
+            count1 = 0;
+
+            if(count2 === n) {
+                series_j2++;
+            }
+        }
+    }
+
+    //En ligne
+    for(i=0;i<3;i++) {
+        count1 = 0;
+        count2 = 0;
+
+        //Horizontalement
+        for(j=0;j<3;j++) {
+            if(game.grid[i][j] === 1) {
+                count1++;
+                count2 = 0;
+
+                if(count1 === n) {
+                    series_j1++;
+                }
+            }
+            else if(game.grid[i][j] === 2) {
+                count2++;
+                count1 = 0;
+
+                if(count2 === n) {
+                    series_j2++;
+                }
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+
+        //Verticalement
+        for(j=0;j<3;j++) {
+            if(game.grid[j][i] === 1) {
+                count1++;
+                count2 = 0;
+
+                if(count1 === n) {
+                    series_j1++;
+                }
+            } else if(game.grid[j][i] === 2) {
+                count2++;
+                count1 = 0;
+
+                if(count2 === n) {
+                    series_j2++;
+                }
+            }
+        }
+    }
+}
+
+function computePossibilities(game) {
+    var winner,
+        nbPawn = 0,
+        i,
+        j;
+
+    //On compte le nombre de pions présents sur le plateau
+    for(i=0 ; i<3 ; i++) {
+        for(j=0 ; j<3 ; j++) {
+            if(game.grid[i][j] !== 0) {
+                nbPawn++;
+            }
+        }
+    }
+
+    if( (winner = getWinner(game)) !== 0) {
+        if(winner === 1) {
+            return 1000 - nbPawn;
+        } else if(winner === 2) {
+            return -1000 + nbPawn;
+        } else {
+            return 0;
+        }
+    }
+
+    //On compte le nombre de séries de 2 pions alignés de chacun des joueurs
+    var series_j1 = 0,
+        series_j2 = 0;
+
+    nb_series(game, series_j1, series_j2, 2);
+
+    return (series_j1 - series_j2);
+}
+
+function getWinner(game) {
+    var i,
+        j,
+        j1,
+        j2;
+
+    nb_series(game, j1, j2, 3);
+
+    if(j1) {
+        return 1;
+    } else if(j2) {
+        return 2;
+    } else {
+        //Si le jeu n'est pas fini et que personne n'a gagné, on renvoie 0
+        for(i=0;i<3;i++) {
+            for(j=0;j<3;j++) {
+                if(game.grid[i][j] === 0) {
+                    return 0;
+                }
+            }
+        }
+    }
+
+    //Si le jeu est fini et que personne n'a gagné, on renvoie 3
+    return 3;
+}
+
+module.exports = nextAction;
