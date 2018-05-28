0f23d70b9eb1b5114a1de3dabf644fc6412753dd
Holdmybeer <Dagobert1108@gmail.com>
2018-05-28 17:37:59 +0200

    IA marche avec quelque bug

--- app/public/morpion/morpion_IA.js
+++ app/public/morpion/morpion_IA.js
@@ -1,37 +1,158 @@
 const Game = require('../Game.js');
 const Action = require('../Action.js');
 
-
-
-var huPlayer = 1,
-    aiPlayer = 2,
-    nbCol = 3,
-    nbLi = 3;
+var board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+var huPlayer = "P";
+var aiPlayer = "C";
+var iter = 0;
+var round = 0;
+var aiCo = "white";
+var huCo = "#333";
+var nbCol = 3;
+var nbLi = 3;
 
 function convertBoard(grid) {
-    var board = [];
 
     for(var v=0 ; v<nbCol ; v++) {
         for(var w=0 ; w<nbLi ; w++) {
-            board[w*nbCol+v] = grid[v][w];
+            if(grid[v][w] === 1) {
+                board[w*nbCol+v] = "P";
+            }
+            if(grid[v][w] === 2) {
+                board[w*nbCol+v] = "C";
+            }
         }
     }
-    return board;
 }
 
 var nextAction = function(game){
 
-    var origBoard = convertBoard(game.grid);
-    var action = minimax(origBoard, 2);
+    convertBoard(game.grid);
+    var index = minimax(board, 'C').index;
+    console.log("index : "+index);
 
-    return action;
+    var y = (index%nbCol),
+        x = parseInt(index/nbCol);
+
+    console.log("x : "+x);
+    console.log("y : "+y);
+
+    return new Action('{"x":'+x+', "y":'+y+', "currentPlayer":2}');
 }
 
-function emptyIndexies(board){
-    return  board.filter(s => s != 1 && s != 2);
+function move(element, player, color) {
+    console.log("element"+ element.id);
+    if (board[element.id] != "P" && board[element.id] != "C") {
+        round++;
+        $(element).css("background-color", color);
+        board[element.id] = player;
+        console.log(board);
+
+        if (winning(board, player)) {
+            setTimeout(function() {
+                alert("YOU WIN");
+                reset();
+            }, 500);
+            return;
+        } else if (round > 8) {
+            setTimeout(function() {
+                alert("TIE");
+                reset();
+            }, 500);
+            return;
+        } else {
+            round++;
+            var index = minimax(board, aiPlayer).index;
+            var selector = "#" + index;
+            $(selector).css("background-color", aiCo);
+            board[index] = aiPlayer;
+            console.log(board);
+            console.log(index);
+            if (winning(board, aiPlayer)) {
+                setTimeout(function() {
+                    alert("YOU LOSE");
+                    reset();
+                }, 500);
+                return;
+            } else if (round === 0) {
+                setTimeout(function() {
+                    alert("tie");
+                    reset();
+                }, 500);
+                return;
+            }
+        }
+    }
 }
 
-function winning(board, player){
+function reset() {
+    round = 0;
+    board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    $("td").css("background-color", "transparent");
+}
+
+function minimax(reboard, player) {
+    iter++;
+    let array = avail(reboard);
+    if (winning(reboard, huPlayer)) {
+        return {
+            score: -10
+        };
+    } else if (winning(reboard, aiPlayer)) {
+        return {
+            score: 10
+        };
+    } else if (array.length === 0) {
+        return {
+            score: 0
+        };
+    }
+
+    var moves = [];
+    for (var i = 0; i < array.length; i++) {
+        var move = {};
+        move.index = reboard[array[i]];
+        reboard[array[i]] = player;
+
+        if (player == aiPlayer) {
+            var g = minimax(reboard, huPlayer);
+            move.score = g.score;
+        } else {
+            var g = minimax(reboard, aiPlayer);
+            move.score = g.score;
+        }
+        reboard[array[i]] = move.index;
+        moves.push(move);
+    }
+
+    var bestMove;
+    if (player === aiPlayer) {
+        var bestScore = -10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    } else {
+        var bestScore = 10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }
+    return moves[bestMove];
+}
+
+//available spots
+function avail(reboard) {
+    return reboard.filter(s => s != "P" && s != "C");
+}
+
+// winning combinations
+function winning(board, player) {
     if (
         (board[0] == player && board[1] == player && board[2] == player) ||
         (board[3] == player && board[4] == player && board[5] == player) ||
@@ -46,85 +167,6 @@ function winning(board, player){
     } else {
         return false;
     }
-}
-
-// the main minimax function
-function minimax(newBoard, player){
-
-    //available spots
-    var availSpots = emptyIndexies(newBoard);
-
-    // checks for the terminal states such as win, lose, and tie
-    //and returning a value accordingly
-    if (winning(newBoard, huPlayer)){
-        return {score:-10};
-    }
-    else if (winning(newBoard, aiPlayer)){
-        return {score:10};
-    }
-    else if (availSpots.length === 0){
-        return {score:0};
-    }
-
-    // an array to collect all the objects
-    var moves = [];
-
-    // loop through available spots
-    for (var i = 0; i < availSpots.length; i++){
-        //create an object for each and store the index of that spot
-        var move = {};
-        move.index = newBoard[availSpots[i]];
-
-        // set the empty spot to the current player
-        newBoard[availSpots[i]] = player;
-
-        /*collect the score resulted from calling minimax
-          on the opponent of the current player*/
-        if (player == aiPlayer){
-            var result = minimax(newBoard, huPlayer);
-            move.score = result.score;
-        }
-        else{
-            var result = minimax(newBoard, aiPlayer);
-            move.score = result.score;
-        }
-
-        // reset the spot to empty
-        newBoard[availSpots[i]] = move.index;
-
-        // push the object to the array
-        moves.push(move);
-    }
-
-    // if it is the computer's turn loop over the moves and choose the move with the highest score
-    var bestMove;
-    if(player === aiPlayer){
-        var bestScore = -10000;
-        for(var i = 0; i < moves.length; i++){
-            if(moves[i].score > bestScore){
-                bestScore = moves[i].score;
-                bestMove = i;
-            }
-        }
-    }else{
-
-        // else loop over the moves and choose the move with the lowest score
-        var bestScore = 10000;
-        for(var i = 0; i < moves.length; i++){
-            if(moves[i].score < bestScore){
-                bestScore = moves[i].score;
-                bestMove = i;
-            }
-        }
-    }
-
-    // return the chosen move (object) from the moves array
-    //return moves[bestMove];
-
-    var x = (moves[bestMove].index%nbCol),
-        y = parseInt(moves[bestMove].index/nbCol);
-
-    return new Action('{"x":'+x+', "y":'+y+', "currentPlayer":2}');
 }
 
 module.exports = nextAction;
