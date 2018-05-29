df3663c406f02528a80684670e32dd83c19bb42e
FofoDev <faustiine35@gmail.com>
2018-05-29 12:05:21 +0200

    ajout p4 server

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ let app = express();
 
 const Game = require('./public/Game.js');
 const Action = require('./public/Action.js');
+
 const logicfunctions = require('./public/morpion/logic.js');
 const morpion_play = logicfunctions.play;
 const morpion_movePossible = logicfunctions.movePossible;
@@ -20,7 +21,7 @@ app.use(bodyParser.json());
 
 // enable use file client
 app.use(express.static('public'));
-app.use('/boostrap',  express.static(__dirname + '/boostrap'));
+app.use('/boostrap', express.static(__dirname + '/boostrap'));
 
 // Add headers
 app.use(function(req, res, next) {
@@ -63,28 +64,47 @@ app.post('/game', function(req, res) {
     // On récupète l'action dans un objet
     let action = new Action(req.body.action);
 
-    // Traitement cas de fin lorsqu'il n'y a plus qu'une case de libre (impossible pour lIA de jouer)
+    let play;
+    let nextAction;
+    let movePossible;
+    let won;
+    let movesLeft;
 
     switch (game.gameType) {
       case 'morpion':
-        if ((morpion_movePossible(game.grid, action)) && (game.winner === 0) && (morpion_movesLeft(game.grid) === 1)) {
-          var humanPlayedGame = morpion_play(game, action);
-          if (morpion_won(game.grid, action.currentPlayer)) {
-            game.winner = action.currentPlayer;
-          }
-          res.send(humanPlayedGame);
-        } else if ((morpion_movePossible(game.grid, action)) && (game.winner == 0)) { // Si le move est possible on joue
+        play = morpion_play;
+        nextAction = morpion_nextAction;
+        movePossible = morpion_movePossible;
+        won = morpion_won;
+        movesLeft = morpion_movesLeft;
+        break;
+      case 'puissance4':
+        play = puissance4_play;
+        nextAction = puissance4_nextAction;
+        movePossible = puissance4_movePossible;
+        won = puissance4_won;
+        movesLeft = puissance4_movesLeft;
+        break;
+        default :
+        break;
+      }
+
+      // Traitement cas de fin lorsqu'il n'y a plus qu'une case de libre (impossible pour lIA de jouer)
+        if ((movePossible(game.grid, action)) && (game.winner == 0)) { // Si le move est possible on joue
 
           // On applique la fonction de jeu sur l'action du joueur
-          let humanPlayedGame = morpion_play(game, action);
+          let humanPlayedGame = play(game, action);
+
+          // Si il ne reste plus de move possible, on ne fait pas jouer l'ia
+          if (movesLeft(humanPlayedGame.grid) === 0)  res.send(humanPlayedGame)
+
           // On calcule l'action de l'IA
-          let iaAction = morpion_nextAction(humanPlayedGame);
+          let iaAction = nextAction(humanPlayedGame);
           // On applique la fonction de jeu sur l'action de l'IA
-          let iaPlayedGame = morpion_play(humanPlayedGame, iaAction);
+          let iaPlayedGame = play(humanPlayedGame, iaAction);
 
           // On vérfie si la partie n'est pas finie
-          if (morpion_won(game.grid, action.currentPlayer))
-            game.winner = action.currentPlayer;
+          if (won(game.grid, action.currentPlayer)) game.winner = action.currentPlayer;
 
           //On renvoie le nouvel état de la partie au client
           res.send(iaPlayedGame);
@@ -92,24 +112,9 @@ app.post('/game', function(req, res) {
           res.send("ERROR");
         }
     }
-    break;
-    case 'puissance4' :
-    // 
-    // // On applique la fonction de jeu sur l'action du joueur
-    // let humanPlayedGame = morpion_play(game, action);
-    // // On calcule l'action de l'IA
-    // let iaAction = morpion_nextAction(humanPlayedGame);
-    // // On applique la fonction de jeu sur l'action de l'IA
-    // let iaPlayedGame = morpion_play(humanPlayedGame, iaAction);
 
-    //On renvoie le nouvel état de la partie au client
-    res.send(game);
-    break;
-    default :
+  });
 
-  }
-
-});
 
 // ------------------------------- LISTEN --------------------------------------
 app.listen(1234, function() {
