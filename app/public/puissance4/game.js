f722f5be2c1da5801cc1f4349e44397f717086c4
Solenn KEROULLAS <keroullasolenn@gmail.com>
2018-05-29 15:04:33 +0200

    FIGTH FOR FREEDOM CSS

--- app/public/puissance4/game.js
+++ app/public/puissance4/game.js
@@ -2,3 +2,144 @@
 
 const Game = require('../Game.js');
 const Action = require('../Action.js');
+
+// -------------------------- GAME VARIABLES -----------------------------------
+
+let currentGame = new Game('puissance4');
+//création du plateau de jeu
+let plateau= [];
+//nuùéro du joueur qui joue
+let turn= 1;
+
+/* un entier:
+   0: la partie continue
+   -1: la partie est nulle
+   1: joueur 1 a gagné
+   2: joueur 2 a gagné
+ */
+let game_status= 0;
+// Nombre de coups joués
+let coups= 0;
+// Nombre de lignes 6
+let lines= 4;
+// Nombre de colonnes 7
+let columns=4;
+const huPlayer="X";
+const aiPlayer="I";
+let iter = 0;
+ const parent=document.querySelector('#jeu');
+let board =[];
+ const t = document.createElement('table');
+  t.id = 'plateau';
+
+// ---------------------- INITIALISATION EVENTS --------------------------------
+
+// Chargement de la page -> création game
+$('document').ready(function() {
+  // On dessine le canvas vide
+  drawInitGame();
+  // On envoie une requête de création de partie
+  $.post("http://localhost:1234/game", {
+    typeGame: 'puissance4'
+  }, function(data) {
+    currentGame.fromJson(JSON.stringify(data));
+  });
+});
+
+
+
+// -------------------------- INIT FUNCTIONS -------------------------------------
+function initBoard(nbcol, nblin){
+  var nbcase= 0;
+  board.length=nbcol*nblin;
+  for (var i=0; i<board.length; i++){
+    board[i]=i;
+  }
+
+}
+
+function drawInitGame() {
+	//initialisation du board de l'ia
+  initBoard(columns, lines);
+
+  //création du plateau de jeu
+  for (var i = lines - 1; i >= 0; i--) {
+    var tr = document.createElement('tr');
+    plateau[i] = [];
+    for (var j = 0; j < columns; j++) {
+      var td = document.createElement('td');
+      td.dataset.column = j;
+      tr.appendChild(td);
+      plateau[i][j] = td;
+    }
+    t.appendChild(tr);
+  }
+  parent.appendChild(t);
+}
+
+ t.addEventListener('click', function(e) { 
+ 	
+ 	 $.post("http://localhost:1234/game", {
+    // On envoie la game actuelle
+    game: JSON.stringify(currentGame.toJson()),
+    // On envoie l'action faite par le joueur
+    action: JSON.stringify(new Action(getActionPlayer(e)))
+  }, function(data) {
+    // Quand on recoit la réponse
+
+    // Si c'est une erreur
+    if (data == "ERROR") {
+      console.log('error');
+    }
+    else {
+
+      // On a recu le nouvel état de la game qu'on stocke dans la variable
+      currentGame.fromJson(JSON.stringify(data));
+      // On dessine le nouvel état de la game
+      draw(currentGame.grid);
+      console.log(data);
+      coups++;
+
+      // Si la partie est finie, on  affiche le gagnant
+      if(currentGame.winner != 0){
+        if(currentGame.winner == 1){
+          alert("joueur " + 1 + " a gagné");
+        }
+        else{
+          alert("L'ordinateur a gagné :(");
+        }
+      }
+    }
+  });
+});
+
+
+function getActionPlayer(event) {
+	var column = event.target.dataset.column;
+	
+	var row;
+  	for (var i = 0; i < lines; i++) {
+    	if (!plateau[i][column].className) {
+      		row = i;
+      		break;
+    	}
+  	}
+  	if (row === undefined) {
+    	window.alert("La colonne est pleine!");
+    	return;
+  	}	
+ // 	console.log( "coordonnées pion "+row + " "+column);
+  return '{"x": ' + column + ',"y": ' + row + ',"currentPlayer": 1}';
+}
+
+// function auxiliaire d'affichage
+ function draw(board) {
+ 	for (var i=0; i<board.length; i++){
+ 		if(board[i]=="X"){
+ 			plateau[parseInt( index/columns)][index%columns].className = 'joueur1';
+ 		}else if (board[i]=="I"){
+ 			plateau[parseInt( index/columns)][index%columns].className = 'joueur2';
+ 		
+ 		}
+ 	}
+}
--- app/public/puissance4/logic.js
+++ app/public/puissance4/logic.js
@@ -1,7 +1,45 @@
 // -------------------------- PLAY FUNCTION ------------------------------------
 let play = function(game, action) {
-  
+  posePion(action.x, action.y, action.currentPlayer, game.grid);
   return game;
 }
 
-module.exports = play;
+module.exports = {play,winning};
+
+
+// -------------------------- GAME VARIABLES -----------------------------------
+// Nombre de lignes 6
+let lines= 4;
+// Nombre de colonnes 7
+let columns=4;
+
+// -------------------------- ACTIONS FUNCTIONS --------------------------------
+function posePion(xMouse, yMouse, currentPlayer, grid) {
+  if (!((grid[((yMouse*columns)+xMouse)]==="X") || (grid[((yMouse*columns)+xMouse)]==="I"))) {
+    grid[((yMouse*columns)+xMouse)] = currentPlayer;
+  }
+}
+
+// -------------------------- RULE WINNING FUNCTIONS ---------------------------
+
+// winning combinations
+function winning(reboard, player) {
+// console.log("je suis la");
+if (
+  (reboard[0]==player && reboard[5]==player && reboard[10]==player && reboard[15]==player )||
+  (reboard[12]==player && reboard[9]==player && reboard [6]==player && reboard[3]==player)||
+  (reboard[0]==player && reboard[1]==player && reboard[2]==player && reboard[3]==player)||
+  (reboard[4]==player && reboard[5]==player && reboard[6]==player && reboard[7]==player)||
+  (reboard[8]==player && reboard[9]==player && reboard[10]==player && reboard[11]==player)||
+  (reboard[12]==player && reboard[13]==player && reboard[14]==player && reboard[15]==player)||
+  (reboard[0]==player && reboard[4]==player && reboard[8]==player && reboard[12]==player )||
+  (reboard[1]==player && reboard[5]==player && reboard[9]==player && reboard[13]==player)||
+  (reboard[2]==player && reboard[6]==player && reboard[10]==player && reboard[14]==player)||
+  (reboard[3]==player && reboard[7]==player && reboard[11]==player && reboard[15]==player)
+//encore des lignes
+  ) {
+  return true;
+} else {
+  return false;
+}
+}
