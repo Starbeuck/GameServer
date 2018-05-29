737020c117ee9de0f37ac49b9fb425d303dfa40a
Holdmybeer <Dagobert1108@gmail.com>
2018-05-29 11:09:05 +0200

    clarification de la classe IA_morpion

--- app/public/morpion/morpion_IA.js
+++ app/public/morpion/morpion_IA.js
@@ -11,7 +11,6 @@ let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 let huPlayer = "P";
 let aiPlayer = "C";
 let iter = 0;
-let round = 0;
 let nbCol = 3;
 let nbLi = 3;
 
@@ -20,10 +19,10 @@ function convertBoard(grid) {
     for(let v=0 ; v<nbCol ; v++) {
         for(let w=0 ; w<nbLi ; w++) {
             if(grid[v][w] === 1) {
-                board[w*nbCol+v] = "P";
+                board[w*nbCol+v] = huPlayer;
             }
             if(grid[v][w] === 2) {
-                board[w*nbCol+v] = "C";
+                board[w*nbCol+v] = aiPlayer;
             }
         }
     }
@@ -32,7 +31,7 @@ function convertBoard(grid) {
 function nextAction(game){
 
     convertBoard(game.grid);
-    let index = minimax(board, 'C').index;
+    let index = minimax(board, aiPlayer).index;
     console.log("index : "+index);
 
     let y = (index%nbCol),
@@ -42,57 +41,6 @@ function nextAction(game){
     console.log("y : "+y);
 
     return new Action('{"x":'+x+', "y":'+y+', "currentPlayer":2}');
-}
-
-function move(element, player, color) {
-    console.log("element"+ element.id);
-    if (board[element.id] != "P" && board[element.id] != "C") {
-        round++;
-        $(element).css("background-color", color);
-        board[element.id] = player;
-        console.log(board);
-
-        if (winning(board, player)) {
-            setTimeout(function() {
-                alert("YOU WIN");
-                reset();
-            }, 500);
-            return;
-        } else if (round > 8) {
-            setTimeout(function() {
-                alert("TIE");
-                reset();
-            }, 500);
-            return;
-        } else {
-            round++;
-            let index = minimax(board, aiPlayer).index;
-            let selector = "#" + index;
-            $(selector).css("background-color", aiCo);
-            board[index] = aiPlayer;
-            console.log(board);
-            console.log(index);
-            if (winning(board, aiPlayer)) {
-                setTimeout(function() {
-                    alert("YOU LOSE");
-                    reset();
-                }, 500);
-                return;
-            } else if (round === 0) {
-                setTimeout(function() {
-                    alert("tie");
-                    reset();
-                }, 500);
-                return;
-            }
-        }
-    }
-}
-
-function reset() {
-    round = 0;
-    board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    $("td").css("background-color", "transparent");
 }
 
 function minimax(reboard, player) {
@@ -118,7 +66,7 @@ function minimax(reboard, player) {
         move.index = reboard[array[i]];
         reboard[array[i]] = player;
 
-        if (player == aiPlayer) {
+        if (player === aiPlayer) {
             let g = minimax(reboard, huPlayer);
             move.score = g.score;
         } else {
@@ -152,20 +100,20 @@ function minimax(reboard, player) {
 
 //available spots
 function avail(reboard) {
-    return reboard.filter(s => s != "P" && s != "C");
+    return reboard.filter(s => s !== huPlayer && s !== aiPlayer);
 }
 
 // winning combinations
 function winning(board, player) {
     if (
-        (board[0] == player && board[1] == player && board[2] == player) ||
-        (board[3] == player && board[4] == player && board[5] == player) ||
-        (board[6] == player && board[7] == player && board[8] == player) ||
-        (board[0] == player && board[3] == player && board[6] == player) ||
-        (board[1] == player && board[4] == player && board[7] == player) ||
-        (board[2] == player && board[5] == player && board[8] == player) ||
-        (board[0] == player && board[4] == player && board[8] == player) ||
-        (board[2] == player && board[4] == player && board[6] == player)
+        (board[0] === player && board[1] === player && board[2] === player) ||
+        (board[3] === player && board[4] === player && board[5] === player) ||
+        (board[6] === player && board[7] === player && board[8] === player) ||
+        (board[0] === player && board[3] === player && board[6] === player) ||
+        (board[1] === player && board[4] === player && board[7] === player) ||
+        (board[2] === player && board[5] === player && board[8] === player) ||
+        (board[0] === player && board[4] === player && board[8] === player) ||
+        (board[2] === player && board[4] === player && board[6] === player)
     ) {
         return true;
     } else {
